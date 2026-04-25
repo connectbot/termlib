9068c4b45bec9d6d614943cdbce43261037ef5d9
Kenny Root <kenny@the-b.org>
2026-04-25 12:41:44 -0700

    Fix deprecation warnings for UI tests

--- lib/src/test/java/org/connectbot/terminal/ReviewModeTest.kt
+++ lib/src/test/java/org/connectbot/terminal/ReviewModeTest.kt
@@ -17,7 +17,7 @@
 package org.connectbot.terminal
 
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performCustomAccessibilityActionWithLabel
 import androidx.test.ext.junit.runners.AndroidJUnit4
